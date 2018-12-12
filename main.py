97fb79a8b6f745af8e4d7e22908fb2be2147282e
nocturnal <foxnezz@gmail.com>
2018-12-12 22:58:45 +0300

    added dataset loading

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("testy is besty")
+import numpy as np
+import sklearn
+# import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+FILENAME = "wifi_localization.txt"
+
+lines = [line.rstrip('\n').split('\t') for line in open(FILENAME)]
+lines = [[int(x) for x in line] for line in lines]
+
+dataset = {}
+dataset['data'] = np.array([line[:-1] for line in lines])
+dataset['target'] = np.array([line[-1] for line in lines])
+
+print(dataset['target'].shape)
